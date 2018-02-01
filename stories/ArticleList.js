a5c9da3b5c47c4e78eab606b3dfee6106832993b
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-01 12:33:33 +0900

    ArticleList완성

--- stories/ArticleList.js
+++ stories/ArticleList.js
@@ -3,8 +3,32 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import ArticleList from '../src/component/ArticleList';
 // import LoginScreen from '../src/component/LoginScreen';
-
+const articles = [
+  {
+    id: 'article0',
+    title: '게시글 제목',
+    createdAt: 1517453812176,
+    nickName: '김승하',
+  },
+  {
+    id: 'article1',
+    title: '게시글 제목 2',
+    createdAt: 1517453812176,
+    nickName: '김승하',
+  },
+];
+const articlesWithLink = articles.map(article => ({
+  ...article,
+  itemProps: {
+    as: 'a',
+    href: 'https://google.com',
+  },
+}), // 객체
+);
 storiesOf('ArticleList', module)
   .add('default', () => (
-    <ArticleList />
+    <ArticleList articles={articles} />
+  ))
+  .add('link', () => (
+    <ArticleList articles={articlesWithLink} />
   ));
