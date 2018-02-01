8d71f6de2ed94d613c3a4934c0cf3f853f752f9a
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-01 11:36:57 +0900

    루트 로그인 시 리다이랙트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter,
-  Route
+  Route,
+  Redirect,
 } from 'react-router-dom';
 import LoginScreenContainer from './containers/LoginScreenContainer';
+import ListPage from './containers/ListPage';
+import withAuth from './hocs/withAuth';
+
+const Home = withAuth(() => (
+  <Redirect to="/list" />
+));
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/list" component={ListPage} />
           <Route path="/login" component={LoginScreenContainer} />
         </div>
       </BrowserRouter>
