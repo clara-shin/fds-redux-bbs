63535106d50357e056c8e9b7ef1edbe5d3cea3c3
Hyejin Shin <enthusiastic.shj@gmail.com>
2018-02-02 11:51:31 +0900

    리액트 임포트

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import 'semantic-ui-css/semantic.min.css';
-import * as firebase from 'firebase';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import * as firebase from 'firebase';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
